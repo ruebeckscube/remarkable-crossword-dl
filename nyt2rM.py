22a112fe017b5b66910493336205de0e8f4e1902
Josh Ruebeck <josh.ruebeck@gmail.com>
2020-11-05 19:35:07 -0600

    initial commit

---
+++ nyt2rM.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+import requests
+import http.cookiejar
+import rmapy.api as rmapi
+import io
+import datetime
+
+XWFOLDERNAME = "Crosswords"
+
+def getNytInfo(dateStart: datetime.date, dateEnd: datetime.date) -> (str,str):
+    params = {'publish_type': 'daily',
+              'sort_order':'asc',
+              'sort_by': 'print_date',
+              'date_start': str(dateStart),
+              'date_end': str(dateEnd),
+              'limit': '100', }
+    url = 'https://nyt-games-prd.appspot.com/svc/crosswords/v3/36569100/puzzles.json'
+    req = requests.get(url,params)
+    req.raise_for_status()
+    return req.json()['results']
+
+
+def downloadNytPdf(puzzId: str) -> requests.Response:
+    cookieJar = http.cookiejar.MozillaCookieJar('nyt-cookies.txt')
+    cookieJar.load()
+
+    url = 'https://www.nytimes.com/svc/crosswords/v2/puzzle/' + puzzId + '.pdf'
+    pdfResponse = requests.get(url,cookies=cookieJar)
+    pdfResponse.raise_for_status()
+
+    return pdfResponse
+
+
+def findOrCreateXwSubFolder(rmClient,folderName) -> rmapi.Folder:
+    crosswordFolder = findOrCreateXwFolder(rmClient)
+    folders = [f for f in rmClient.get_meta_items()
+               if f.VissibleName == folderName
+               and f.Parent == crosswordFolder.ID
+               and isinstance(f,rmapi.Folder)]
+    destFolder = None
+    if len(folders) == 0:
+        destFolder = rmapi.Folder(folderName)
+        destFolder.Parent = findOrCreateXwFolder(rmClient).ID
+        rmClient.create_folder(destFolder)
+    else:
+        destFolder = folders[0]
+    return destFolder
+
+
+def findOrCreateXwFolder(rmClient) -> rmapi.Folder:
+    folders = [ f for f in rmClient.get_meta_items()
+                        if f.VissibleName == XWFOLDERNAME
+                        and f.Parent != "trash"
+                        and isinstance(f,rmapi.Folder) ]
+    crosswordFolder = None
+    if len(folders)== 0:
+        crosswordFolder = rmapi.Folder(XWFOLDERNAME)
+        rmClient.create_folder(crosswordFolder)
+    else:
+        crosswordFolder = folders[0]
+    return crosswordFolder
+
+def docExists(rmClient,docName,folder) -> bool:
+    docs = [ d for d in rmClient.get_meta_items()
+                     if d.VissibleName == docName
+                     and d.Parent == folder.ID
+                     and isinstance(d,rmapi.Document) ]
+    return (len(docs)>0)
+
+class ZipDocFromBytesIO(rmapi.ZipDocument):
+    def __init__(self,name: str, IO: io.BytesIO, fileType: str):
+        super().__init__()
+        if fileType=="pdf":
+            self.content["fileType"] = "pdf"
+            self.pdf=IO
+        if fileType=="epub":
+            self.content["fileType"] = "epub"
+            self.epub=IO
+        self.metadata["VissibleName"] = name
+
+def downloadNyt(dateStart: datetime.date, dateEnd: datetime.date):
+    rmClient = rmapi.Client()
+    rmClient.renew_token()
+
+    destFolder = findOrCreateXwSubFolder(rmClient,"New York Times")
+
+    if dateEnd is None:
+        dateEnd = datetime.date.today() + datetime.timedelta(1)
+    if dateStart is None:
+        dateStart = None
+    nytInfo=getNytInfo(dateStart,dateEnd)
+    puzzId = str(nytInfo[0]['puzzle_id'])
+    printDate = str(nytInfo[0]['print_date'])
+
+    name=printDate
+    if docExists(rmClient,name,destFolder):
+        print("Already downloaded")
+        return
+    pdfResponse = downloadNytPdf(puzzId)
+    doc = ZipDocFromBytesIO(name,io.BytesIO(pdfResponse.content),"pdf")
+    rmClient.upload(doc,destFolder)
+    
+if __name__=="__main__":
+    downloadNyt()
