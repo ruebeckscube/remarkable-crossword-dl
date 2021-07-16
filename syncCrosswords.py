dc2fe8f70cbdd21777e1843e7cb1e5ea5e3838ae
Travis Carr <tcarr@twitter.com>
2021-07-16 11:21:11 -0400

    Add argument parsing to enable a -t (--today) to simplify usage for users

--- syncCrosswords.py
+++ syncCrosswords.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
+import argparse
 import nyt2rM
 import datetime
 import sys
 
-dateStart,dateEnd = None,None
-if len(sys.argv) > 1: dateStart = datetime.date.fromisoformat(sys.argv[1])
-if len(sys.argv) > 2: dateEnd = datetime.date.fromisoformat(sys.argv[2])
+parser = argparse.ArgumentParser(description="Syncs a copy of the New York Times Crossword to your Remarkable")
+parser.add_argument("start", help="Start Date in ISO format (YYYY-MM-DD)", default=None, nargs='?')
+parser.add_argument("end", help="End Date in ISO format (YYYY-MM-DD)", default=None, nargs='?')
+parser.add_argument("-t", "--today", help="Just get today's crossword. NOTE: Will ignore start/end arguments.", action='store_true')
+args = parser.parse_args()
+
+if not args.today:
+  dateStart = datetime.date.fromisoformat(args.start)
+  dateEnd = datetime.date.fromisoformat(args.end)
+else:
+  dateStart = datetime.date.today()
+  dateEnd = None
+
 nyt2rM.downloadNytCrosswords(dateStart,dateEnd)
